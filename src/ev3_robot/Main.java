3c8857cb1e6cf402385efda5c48a82327f82024c
shelby <s.koch.317@gmail.com>
2020-10-14 00:17:08 -0500

    Adding items to main robot class

--- src/ev3_robot/Main.java
+++ src/ev3_robot/Main.java
@@ -5,14 +5,9 @@ import java.util.concurrent.TimeUnit;
 public class Main{
     public static void main(String[] args) throws Exception {
     	Robot robot = Robot.getInstance();
-        robot.RightTrack.forward();
-        robot.LeftTrack.forward();
-        robot.Claw.backward();
-        TimeUnit.SECONDS.sleep(4);
-        robot.Claw.forward();
-        TimeUnit.SECONDS.sleep(4);
-        robot.Claw.stop();
-        robot.LeftTrack.stop();
-        robot.RightTrack.stop();
+    	robot.OpenClaw();
+    	robot.CloseClaw();
+    	robot.CloseClaw();
+    	robot.OpenClaw();
     }
 }
--- src/ev3_robot/Robot.java
+++ src/ev3_robot/Robot.java
@@ -1,5 +1,6 @@
 package ev3_robot;
 
+import lejos.hardware.Sound;
 import lejos.hardware.motor.BaseRegulatedMotor;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
@@ -9,29 +10,99 @@ import lejos.hardware.port.SensorPort;
 
 public class Robot {
 	
+	//Global instance
 	private static Robot robot_instance = new Robot();
 	
 	//Motors
-	public final BaseRegulatedMotor RightTrack; 
-	public final BaseRegulatedMotor LeftTrack; 
-	public final BaseRegulatedMotor Claw;
+	private final BaseRegulatedMotor rightTrack; 
+	private final BaseRegulatedMotor leftTrack; 
+	private final BaseRegulatedMotor claw;
 	
 	//Sensors
-	public final EV3IRSensor IR;
+	public final EV3IRSensor IRSensor;
+	//public final EV3ColorSensor ColorSensor;
+	//public final EV3UltrasonicSensor UltrasonicSensor;
+	
+	//States
+	private ClawState clawState;
 	
 	
+	//State Enumerators
+	private enum ClawState {
+		OPENED,
+		CLOSED
+	}
+
 	
 	private Robot()
 	{
-		RightTrack = new EV3LargeRegulatedMotor(MotorPort.A);
-		LeftTrack = new EV3LargeRegulatedMotor(MotorPort.B);
-		Claw = new EV3MediumRegulatedMotor(MotorPort.D);
-		IR = new EV3IRSensor(SensorPort.S1);
+		//Initialize motors
+		rightTrack = new EV3LargeRegulatedMotor(MotorPort.A);
+		leftTrack = new EV3LargeRegulatedMotor(MotorPort.B);
+		claw = new EV3MediumRegulatedMotor(MotorPort.D);
+		
+		//Initialize sensors
+		IRSensor = new EV3IRSensor(SensorPort.S1);
+		//UltrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
+		//ColorSensor = new EV3ColorSensor(SensorPort.S3);
+		
+		//Set States 
+		SetClawState(ClawState.OPENED);
+		
 		
 	}
 	
+	///// PUBLIC FUNCTIONS ///////////
+	
+	//Returns instance of Robot object to 
 	public static Robot getInstance()
 	{
 		return robot_instance;
+	}
+	
+	//Methods
+	public void Forward() {
+		//Add code to move robot forward, include parameters for speed, duration, etc...
+	}
+	
+	public void Backward() {
+		//Add code to move robot backward, include parameters for speed, duration, etc...
+	}
+	
+	public void RotateClockwise() {
+		
+	}
+	
+	public void OpenClaw()
+	{
+		if(clawState == ClawState.CLOSED)
+		{
+			claw.setSpeed(claw.getMaxSpeed());
+			claw.rotate(1650);
+			clawState = ClawState.OPENED;
+		}
+		else
+			Sound.twoBeeps();
+			
+	}
+	
+	public void CloseClaw()
+	{
+		if(clawState == ClawState.OPENED)
+		{
+			claw.setSpeed(claw.getMaxSpeed());
+			claw.rotate(-1650);
+			clawState = ClawState.CLOSED;
+		}
+		else
+			Sound.twoBeeps();
+			
+	}
+	
+	/////// PRIVATE FUNCTIONS //////////
+
+	private void SetClawState(ClawState state)
+	{
+		clawState = state;
 	}
 }
