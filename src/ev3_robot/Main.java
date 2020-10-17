60742e077e2abddd73ef86a0630c5118fcc75aae
shelby <s.koch.317@gmail.com>
2020-10-16 22:36:00 -0500

    Cleaning and commenting code before submitting a02

--- src/ev3_robot/Main.java
+++ src/ev3_robot/Main.java
@@ -8,23 +8,14 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
-
 import lejos.hardware.lcd.GraphicsLCD;
 
 public class Main{
     public static void main(String[] args) throws Exception {
-    	Robot robot = Robot.getInstance();
-    	/*robot.OpenClaw();
-    	robot.CloseClaw();
-    	robot.CloseClaw();
-    	robot.OpenClaw();*/
-    	
-    	//robot.Pilot.travel(30.48);
-    	//robot.Pilot.rotate(-360);
-    	//robot.Pilot.backward();
+    	//Instantiate and run move thread
+    	//Robot movement and sensor readings are on different threads so they can run simultaneously
     	MoveThread moveThread = new MoveThread();
     	moveThread.run();
-	
     }
 }
 
@@ -32,10 +23,13 @@ class MoveThread extends Thread {
 	
 	public void run() { 
 		try { 
+			//Get robot instance
 			Robot robot = Robot.getInstance();
+			//Set travel to length of game field
 			robot.Pilot.travel(100); 
-	    	DisplayThread displayThread = new DisplayThread();
-	    	displayThread.run();  
+			//Instantiate and run logging thread
+			LogThread logThread = new LogThread();
+			logThread.run();  
 			} 
 		catch (Exception e) { 
 				System.out.println("Exception is caught on travel thread"); 
@@ -43,21 +37,27 @@ class MoveThread extends Thread {
 	} 
 }
 
-class DisplayThread extends Thread { 
+class LogThread extends Thread { 
 	public void run() { 
 		try { 
+			//Get robot instance
 			Robot robot = Robot.getInstance();
-	    	float[] sample = new float[1];
+			//Create float array to store values from sensor
+			float[] sample = new float[1];
 	    	
+			//File and buffer writer to store sensor values in a text log
 	    	FileWriter fw = new FileWriter("log3.txt", true);
 	        BufferedWriter bw = new BufferedWriter(fw);
 	        PrintWriter out = new PrintWriter(bw);
 	    	
+	        //Run manual loop to get multiple sensor readings
 	    	for(int i = 0; i < 100; i++)
 	    	{
+	    		//Fetch sensor sample and write to text log
 	    		robot.IRSensor.fetchSample(sample, 0);
 	    		out.println(String.valueOf(sample[0]));
 	    	}    		
+	    	//Close file stream
 	    	out.close();
 			} catch (Exception e) 
 		{ 
